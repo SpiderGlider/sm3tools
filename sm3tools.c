e82c45c8c805c1a665d9aa132589b90d4d139cb7
SpiderGlider <SpiderGlider@noreply.codeberg.org>
2025-05-17 19:21:17 +0100

    check file extension of input string
    https://stackoverflow.com/a/5309508

--- sm3tools.c
+++ sm3tools.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello world");
+enum FILE_TYPES {
+    PCPACK,
+    PCSSB
+};
+
+enum FILE_TYPES getFileType(const char* fileName) {
+    char *const fileExtension = strrchr(fileName, '.');
+    //printf("%s", fileExtension);
+
+    // TODO could check magic numbers as well
+
+    // FIXME case sensitive currently
+
+    if (strcmp(fileExtension, ".PCPACK") == 0) return PCPACK;
+    if (strcmp(fileExtension, ".pcssb") == 0) return PCSSB;
+}
+
+int main(int argc, char* argv[]) {
+    getFileType(argv[1]);
+
+    return 0;
 }
