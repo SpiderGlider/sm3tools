b53aba9a94c999edc09597173dea06d5f4dddcf3
SpiderGlider <SpiderGlider@noreply.codeberg.org>
2025-05-17 23:21:53 +0100

    rough draft of one pcssb file reading
    seems to work but ofc this isn't good code.

--- pcssb.c
+++ pcssb.c
@@ -1,2 +1,28 @@
 #include "pcssb.h"
 
+#include <stdio.h>
+
+struct FSB readFile(const char* fileName) {
+    struct FSB fsb = {};
+    FILE *fileHandle = fopen(fileName, "rb");
+
+    uint32_t buffer[6] = {};
+    fread(buffer, 4, 6, fileHandle);
+
+    fsb.fsb3Header = buffer[0];
+    fsb.numFiles = buffer[1];
+    fsb.unknown1 = buffer[2];
+    fsb.dataSize = buffer[3];
+    fsb.unknown2 = buffer[4];
+    fsb.null1 = buffer[5];
+
+    fclose(fileHandle);
+
+    return fsb;
+}
+
+int main(int argc, char* argv[]) {
+    struct FSB fsb = readFile(argv[1]);
+    printf("%u", fsb.unknown1);
+}
+
