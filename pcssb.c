825bdd069f35f99649f5880c0777ac09f624b698
SpiderGlider <SpiderGlider@noreply.codeberg.org>
2025-05-20 20:54:07 +0100

    try printing out FSB3 header
    seems to work...

--- pcssb.c
+++ pcssb.c
@@ -6,9 +6,9 @@ struct FSB readFile(const char* fileName) {
     struct FSB fsb = {};
     FILE *fileHandle = fopen(fileName, "rb");
 
-    uint32_t buffer[6] = {};
+    const size_t count = 100;
+    uint32_t buffer[100] = {};
 
-    const size_t count = 6;
     const size_t numRead = fread(buffer, 4, count , fileHandle);
     if (numRead < count) {
         printf("LOG: count was %lu, amount read was only %lu.\n", count, numRead);
@@ -28,6 +28,6 @@ struct FSB readFile(const char* fileName) {
 
 int main(int argc, char* argv[]) {
     const struct FSB fsb = readFile(argv[1]);
-    printf("%u", fsb.unknown1);
+    printf("%s", (char*) &fsb.fsb3Header);
 }
 
