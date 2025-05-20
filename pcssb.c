3a4d5d72a31c95d063e9c3ac4a81bbbca8f3874a
SpiderGlider <SpiderGlider@noreply.codeberg.org>
2025-05-20 21:13:33 +0100

    attempt findFSBHeaderIndexes
    decided to go this way of dealing with an array in a function for now
    
    https://stackoverflow.com/questions/11656532/returning-an-array-using-c
    
    guess I could use a non null terminated char array for the fsb header string but idk, i feel like this way is cleaner somehow

--- pcssb.c
+++ pcssb.c
@@ -2,6 +2,46 @@
 
 #include <stdio.h>
 
+//prints out indexes of every instance of "FSB3" header text in the sound file
+size_t findFSBHeaderIndexes(
+    const char *const inputFileName,
+    char *const resultArr,
+    const size_t resultArrLen) {
+
+    //next index in resultArr to fill
+    size_t resultCount = 0;
+
+    FILE *fileHandle = fopen(inputFileName, "rb");
+
+    const size_t buffSize = 100;
+    //magic number needs to be reused to avoid Variable Length Array
+    char buffer[100] = {};
+
+    const size_t numRead = fread(buffer, 1, buffSize , fileHandle);
+    if (numRead < buffSize) {
+        printf("LOG: count was %lu, amount read was only %lu.\n", buffSize, numRead);
+    }
+
+    //string to match in the file (except for null termination)
+    const char fsbHeaderString[5] = "FSB3";
+    //represents current character in the string to match
+    int fsbStrIndex = 0;
+    for (size_t i = 0; i < numRead; i++) {
+        if (buffer[i] == fsbHeaderString[fsbStrIndex]) {
+            fsbStrIndex++;
+            //if matched (ignore \0 character in fsbHeaderString)
+            if (fsbStrIndex == 4) {
+                fsbStrIndex = 0;
+                //FIXME: add length checking
+                resultArr[resultCount] = buffer[i];
+                resultCount++;
+            }
+        }
+    }
+    //FIXME return more useful value which represents if resultArr was too small
+    return resultCount;
+}
+
 struct FSB readFile(const char* fileName) {
     struct FSB fsb = {};
     FILE *fileHandle = fopen(fileName, "rb");
